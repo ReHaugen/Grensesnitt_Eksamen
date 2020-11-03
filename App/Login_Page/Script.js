54e8ea3f341782f1947859632be7e0db2f55f07b
ReHaugen <Reidar_Haugen@outlook.com>
2020-11-03 14:48:34 +0100

    Rework LoginPage

--- App/Login_Page/Script.js
+++ App/Login_Page/Script.js
@@ -1,15 +1,52 @@
-const name = document.getElementById("uName");
-const password = document.getElementById("pass");
-const form = document.getElementById("form");
-const error = document.getElementById("error");
+//GETTING INPUTS
+var username = document.forms["vform"]["username"];
+var password = document.forms["vform"]["password"];
 
-form.addEventListener('submit', (e) => {
-    let messages = [];
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required');
+
+//GETTING ERROR DISPLAY
+var name_error = document.getElementById("name_error");
+var password_error = document.getElementById("password_error");
+
+//SETTING EVENT LISTENERS
+username.addEventListener("blur", nameVerify, true);
+password.addEventListener("blur", passwordVerify, true);
+
+//Validate function
+function Validate() {
+    //Username validation
+    if (username.value == "") {
+        username.style.border = "1px solid red";
+        name_error.textContent = "Username is required";
+        username.focus();
+        return false
+
+    }
+    //Password validation
+    if (password.value == "") {
+        password.style.border = "1px solid red";
+        password_error.textContent = "Password is required";
+        password.focus();
+        return false
+
     }
 
-    
+}
+//EVENT HANDLER
+function nameVerify() {
+    if (username.value != "") {
+        username.style.border = "1px solid #5E6E66;";
+        name_error.innerHTML = "";
+        return true;
 
-   
-});
+    }
+}
+function passwordVerify() {
+    if (password.value != "") {
+        password.style.border = "1px solid #5E6E66;";
+        password_error.innerHTML = "";
+        return true;
+
+    }
+}
+
+
