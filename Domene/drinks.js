83b40ea95f7a9e6661a0001731d0d52fe6f5f405
Juni Halseth Stenberg <juni@lyse.net>
2020-11-04 20:58:00 +0100

    Add drink to order

---
+++ Domene/drinks.js
@@ -0,0 +1,36 @@
+export default {
+    "filterkaffe": {
+        name: "Filterkaffe",
+        size: {
+            small: {
+                name: "liten",
+                price: 35.00
+            },
+            medium: {
+                name: "medium",
+                price: 45.00
+            },
+            large: {
+                name: "stor",
+                price: 55.00
+            }
+        }  
+    },
+    "cappuccino": {
+        name: "Cappucccino",
+        size: {
+            small: {
+                name: "liten",
+                price: 35.00
+            },
+            medium: {
+                name: "medium",
+                price: 45.00
+            },
+            large: {
+                name: "stor",
+                price: 55.00
+            }
+        }  
+    },
+}
