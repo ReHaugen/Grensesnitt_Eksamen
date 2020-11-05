44a5c0cb7d16cd248a25d03410ac1ac3bdde24e0
Juni Halseth Stenberg <juni@lyse.net>
2020-11-05 17:39:46 +0100

    Add drinks and desserts to datatype

--- Domene/drinks.js
+++ Domene/drinks.js
@@ -4,16 +4,20 @@ export default {
         size: {
             small: {
                 name: "liten",
-                price: 35.00
+                price: 28.00
             },
             medium: {
                 name: "medium",
-                price: 45.00
+                price: 32.00
             },
             large: {
                 name: "stor",
-                price: 55.00
+                price: 36.00
             }
+        },
+        image: {
+            src: "filterkaffe.jpeg",
+            alt: "Bilde av filterkaffe"
         }  
     },
     "cappuccino": {
@@ -21,16 +25,188 @@ export default {
         size: {
             small: {
                 name: "liten",
-                price: 35.00
+                price: 38.00
             },
             medium: {
                 name: "medium",
+                price: 42.00
+            },
+            large: {
+                name: "stor",
+                price: 46.00
+            }
+        }, 
+        image: {
+            src: "cappuccino.jpeg",
+            alt: "Bilde av cappuccino"
+        }   
+    },
+    "americano": {
+        name: "Americano",
+        size: {
+            small: {
+                name: "liten",
+                price: 32.00
+            },
+            medium: {
+                name: "medium",
+                price: 36.00
+            },
+            large: {
+                name: "stor",
+                price: 40.00
+            }
+        }, 
+        image: {
+            src: "americano.jpeg",
+            alt: "Bilde av americano"
+        }   
+    },
+    "iskaffe": {
+        name: "Iskaffe",
+        size: {
+            small: {
+                name: "liten",
+                price: 36.00
+            },
+            medium: {
+                name: "medium",
+                price: 42.00
+            },
+            large: {
+                name: "stor",
+                price: 50.00
+            }
+        }, 
+        image: {
+            src: "iskaffe.jpeg",
+            alt: "Bilde av iskaffe"
+        }   
+    },
+    "caffe mocha": {
+        name: "Caffe mocha",
+        size: {
+            small: {
+                name: "liten",
+                price: 38.00
+            },
+            medium: {
+                name: "medium",
+                price: 42.00
+            },
+            large: {
+                name: "stor",
+                price: 46.00
+            }
+        }, 
+        image: {
+            src: "caffe mocha.jpeg",
+            alt: "Bilde av caffe mocha"
+        }   
+    },
+    "caffe latte": {
+        name: "Caffe latte",
+        size: {
+            small: {
+                name: "liten",
+                price: 38.00
+            },
+            medium: {
+                name: "medium",
+                price: 44.00
+            },
+            large: {
+                name: "stor",
+                price: 48.00
+            }
+        }, 
+        image: {
+            src: "caffe latte.jpeg",
+            alt: "Bilde av caffe latte"
+        }   
+    },
+    "caramel macchiato": {
+        name: "Caramel macchiato",
+        size: {
+            small: {
+                name: "liten",
                 price: 45.00
+            },
+            medium: {
+                name: "medium",
+                price: 48.00
+            },
+            large: {
+                name: "stor",
+                price: 51.00
+            }
+        }, 
+        image: {
+            src: "caramel macchiato.jpeg",
+            alt: "Bilde av caramel macchiato"
+        }   
+    },
+    "espresso": {
+        name: "Espresso",
+        size: {
+            small: {
+                name: "liten",
+                price: 34.00
+            },
+            medium: {
+                name: "medium",
+                price: 38.00
+            },
+            large: {
+                name: "stor",
+                price: 42.00
+            }
+        }, 
+        image: {
+            src: "espresso.jpeg",
+            alt: "Bilde av espresso"
+        }   
+    },
+    "cortado": {
+        name: "Cortado",
+        size: {
+            small: {
+                name: "liten",
+                price: 36.00
+            },
+            medium: {
+                name: "medium",
+                price: 40.00
+            },
+            large: {
+                name: "stor",
+                price: 44.00
+            }
+        }, 
+        image: {
+            src: "cortado.jpeg",
+            alt: "Bilde av cortado"
+        }   
+    },
+    "iste": {
+        name: "Iste",
+        size: {
+            small: {
+                name: "liten",
+                price: 38.00
+            },
+            medium: {
+                name: "medium",
+                price: 47.00
             },
             large: {
                 name: "stor",
                 price: 55.00
             }
-        }  
-    },
+        }, 
+        image: {
+            src: "iste.jpeg",
+            alt: "Bilde av iste"
+        }   
+    }
 }
