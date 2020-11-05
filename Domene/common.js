e3cb13ae87e563b22669784d63a49805b47204f3
Juni Halseth Stenberg <juni@lyse.net>
2020-11-05 17:57:35 +0100

    Add a bit of documentation to common functions

--- Domene/common.js
+++ Domene/common.js
@@ -1,25 +1,28 @@
 import drinks from './drinks.js';
+import desserts from './dessert.js';
 
+// Constants
 const localStorageOrdersName = "orders";
 const localStorageUserIdName = "userId";
 
-export function addDrinkToCart(name, size) {
-    const drink = getDrink(name);
-    addToCart(getUserId() ,{
-        key: drink.name,
-        price: drink.size[size].price,
-        description: `${drink.name} - ${drink.size[size].name}`
-    });
-}
 
-function getDrink(name) {
-    return drinks[name];
-}
 
+// User management
+
+/**
+ * Get the current 'logged in' user. 
+ * Currently just a string from local storage.
+ */
 function getUserId() {
     return localStorage.getItem(localStorageUserIdName);
 }
 
+/**
+ * Sets the current user id to local storage and prepares a new order.
+ * Can also be used to clear an order since it cleares the order for the specified user.
+ * 
+ * @param {string} userId - can be anything; a nickname, full name or email
+ */
 export function setUserId(userId) {
     localStorage.setItem(localStorageUserIdName, userId)
     localStorage.setItem(localStorageOrdersName, JSON.stringify({
@@ -30,8 +33,44 @@ export function setUserId(userId) {
     }));
 }
 
+// Drink management
+
+/**
+ * Adds a drink to the cart for the current logged in user.
+ * 
+ * @param {string} name name of the drink how it appears in the drinks.js data structure
+ * @param {string} size size of the drink. Allowed values are: small, medium, large
+ */
+export function addDrinkToCart(name, size) {
+    const drink = getDrink(name);
+    addToCart(getUserId() ,{
+        key: drink.name,
+        price: drink.size[size].price,
+        description: `${drink.name} - ${drink.size[size].name}`
+    });
+}
+
+/**
+ * Gets a drink by name from the drinks.js data structure.
+ * 
+ * @param {string} name name of the drink how it appears in the drinks.js data structure
+ */
+function getDrink(name) {
+    return drinks[name];
+}
+
+
+// Cart management
+
+/**
+ * Add a generic item to the cart and calculates the current total.
+ * Currently *not* incrementing the quantity if duplicate items are added.
+ * 
+ * @param {string} userId to which user id to add to its cart
+ * @param {string} item either a drink (drink.js) or dessert (dessert.js) 
+ */
 function addToCart(userId, item) {
-    const order = getOrdersByUserId(userId);
+    const order = getOrderByUserId(userId);
     const orderLines = [...order?.orderLines ?? [], {
         key: item.key,
         quantity: 1,
@@ -50,11 +89,19 @@ function addToCart(userId, item) {
     );
 }
 
-function getOrdersByUserId(userId) {
+/**
+ * Gets the current order for the specified user from local storage.
+ * 
+ * @param {string} userId to which user it concerns
+ */
+function getOrderByUserId(userId) {
     const orders = JSON.parse(localStorage.getItem(localStorageOrdersName));
     return orders[userId];
 }
 
+/**
+ * Get all orders from the local storage
+ */
 function getOrders() {
     return JSON.parse(localStorage.getItem(localStorageOrdersName));
 }
