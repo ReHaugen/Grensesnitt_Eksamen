1062916d85f75c5f6a0bfb5248637f2267986bf6
Juni Halseth Stenberg <juni@lyse.net>
2020-11-07 15:48:12 +0100

    order on kassasystem

---
+++ Kassesystem/Order_Page/script-pay-order.js
@@ -0,0 +1,146 @@
+import {
+  removeOrderLineForLoggedInUser,
+  getCurrentTotalForLoggedInUser,
+  getOrderLinesForLoggedInUser,
+  getOrderLineForLoggedInUser,
+  updateOrderLineQuantityForLoggedInUser,
+  changeOrderStatusToInProgress,
+  getOrderKeyForLoggedInUser,
+} from "../../Domene/common.js";
+
+// Set the current total on page load
+const totalElement = document.querySelector("#total");
+totalElement.innerHTML = `${getCurrentTotalForLoggedInUser()}`;
+
+const orderLinesElement = document.querySelector("#orderLines");
+let orderLines = getOrderLinesForLoggedInUser();
+orderLines.forEach((orderLine) => {
+  const orderLineElement = createOrderLineElement(orderLine);
+  orderLinesElement.appendChild(orderLineElement);
+});
+
+payEventListener();
+
+/**
+ * Creates an order line element.
+ * @param {*} orderLine an order line from the order in local storage
+ */
+export function createOrderLineElement(orderLine) {
+  const element = document.createElement("div");
+  element.classList.add("orderLine");
+
+  const titleElement = document.createElement("div");
+  titleElement.classList.add("normaltext");
+  titleElement.innerHTML = `${orderLine.quantity}x ${orderLine.description}, ${orderLine.price} kr`;
+
+  const xButton = document.createElement("button");
+  xButton.classList.add("small-button");
+  xButton.innerHTML = "x";
+
+  // Remove the order line, update the total and remove the entire order line event when clicked
+  xButton.onclick = () => {
+    removeOrderLineForLoggedInUser(orderLine.key);
+    orderLinesElement.removeChild(element);
+    updateTotal();
+  };
+
+  const subtractButton = document.createElement("button");
+  subtractButton.classList.add("small-button");
+  subtractButton.innerHTML = "-";
+
+  // Subtract one from the order line
+  subtractButton.onclick = () => {
+    const subtractedByOne = orderLine.quantity - 1;
+    if (subtractedByOne !== 0) {
+      updateOrderLineQuantityForLoggedInUser(orderLine.key, subtractedByOne);
+      const refreshedOrderLine = getOrderLineForLoggedInUser(orderLine.key);
+      replaceOrderLineEvent(element, refreshedOrderLine);
+    } else {
+      removeOrderLineForLoggedInUser(orderLine.key);
+      orderLinesElement.removeChild(element);
+      orderLines = getOrderLinesForLoggedInUser();
+      payEventListener();
+    }
+    updateTotal();
+  };
+
+  const addButton = document.createElement("button");
+  addButton.classList.add("small-button");
+  addButton.innerHTML = "+";
+
+  // Add one to the order line
+  addButton.onclick = () => {
+    const addedOne = orderLine.quantity + 1;
+    updateOrderLineQuantityForLoggedInUser(orderLine.key, addedOne);
+    const refreshedOrderLine = getOrderLineForLoggedInUser(orderLine.key);
+    replaceOrderLineEvent(element, refreshedOrderLine);
+    updateTotal();
+  };
+
+  element.appendChild(titleElement);
+  element.appendChild(subtractButton);
+  element.appendChild(addButton);
+  element.appendChild(xButton);
+
+  return element;
+}
+
+/**
+ * Refreshes the state and replaces the element with a new one based of the new order line
+ */
+function replaceOrderLineEvent(element, orderLine) {
+  orderLinesElement.replaceChild(createOrderLineElement(orderLine), element);
+}
+
+/**
+ * Updates the total
+ */
+function updateTotal() {
+  totalElement.innerHTML = `${getCurrentTotalForLoggedInUser()}`;
+}
+
+// Add or replace event listeners to pay buttons
+function payEventListener() {
+  document.querySelectorAll("[name=pay]").forEach((element) => {
+    if (orderLines.length == 0) {
+      element.style.cursor = "not-allowed";
+    } else {
+      element.style.cursor = "pointer";
+    }
+    element.onclick = () => payForOrder();
+  });
+}
+
+function payForOrder() {
+  if (orderLines.length == 0) {
+    alert("Du må legge noe inn i handlekurven for å kunne betale");
+  } else {
+    const consentToPay = confirm(
+      `Er du sikker på at du vil betale ${getCurrentTotalForLoggedInUser()} kr for orderen?`
+    );
+    if (consentToPay) {
+      changeOrderStatusToInProgress();
+      clearPage();
+    }
+  }
+}
+
+function clearPage() {
+  while (orderLinesElement.firstChild) {
+    orderLinesElement.removeChild(orderLinesElement.lastChild);
+  }
+  updateTotal();
+}
+
+/**
+ * Refresh the entire shopping cart.
+ */
+export function refreshShoppingCart() {
+  orderLines = getOrderLinesForLoggedInUser();
+  payEventListener();
+  clearPage();
+  orderLines.forEach((orderLine) => {
+    const orderLineElement = createOrderLineElement(orderLine);
+    orderLinesElement.appendChild(orderLineElement);
+  });
+}
---
+++ Kassesystem/Order_Page/script-prepare-order.js
@@ -0,0 +1,174 @@
+import {
+  checkLoginOrRedirectKasseSystem,
+  addDrinkToCart,
+  addDessertToCart,
+  getDrink,
+  getDessert,
+} from "../../Domene/common.js";
+checkLoginOrRedirectKasseSystem();
+
+import { refreshShoppingCart } from './script-pay-order.js'
+
+let orderQuantity = 1;
+
+const displayMenuCard = document.getElementById("menuCard");
+const closeButton = document.getElementsByClassName("close")[0];
+
+const menuPhoto = document.getElementsByClassName("btnPhoto");
+const itemPhoto = document.getElementById("photoContainer");
+const itemName = document.getElementById("itemTitle");
+const foodPriceDiv = document.getElementById("foodPrice");
+const foodPriceTxt = document.getElementById("pricetxt");
+const drinkSize = document.getElementById("changeSizeContainer");
+const smalltext = document.getElementById("smalltxt");
+const mediumtext = document.getElementById("mediumtxt");
+const largetext = document.getElementById("largetxt");
+const addBtn = document.getElementById("add_btn");
+const displayQuantity = document.getElementById("quantityCounter");
+const subtractBtn = document.getElementById("subtract_btn");
+const drinkButtons = document.querySelectorAll(".drink-button");
+const foodButtons = document.querySelectorAll(".food-button");
+
+// Event listeners
+addBtn.onclick = addQuantity;
+subtractBtn.onclick = subtractQuantity;
+
+// Add event listener onclick. Get the drink from the data structure and open modal.
+drinkButtons.forEach((drinkbutton) =>
+  drinkbutton.addEventListener("click", (click) =>
+    openDrinkModal(drinkbutton.name)
+  )
+);
+
+// Add event listener onclick. Get the dessert from the data structure and open modal.
+foodButtons.forEach((foodButton) =>
+  foodButton.addEventListener("click", (click) =>
+    openDessertModal(foodButton.name)
+  )
+);
+
+/**
+ * Opens the drinks modal.
+ * @param {string} name name of the drink as it is in the drinks.js
+ */
+function openDrinkModal(name) {
+  const drink = getDrink(name);
+
+  orderQuantity = 1;
+  displayQuantity.innerHTML = `${orderQuantity}`;
+  itemName.innerHTML = drink.name;
+  drinkSize.style.display = "block";
+  foodPrice = 0;
+  foodPriceDiv.style.display = "none";
+  itemPhoto.innerHTML = `<img src="Images/${drink.image.src}" alt="${drink.image.alt}" id = "menuPhoto" />`;
+
+  smalltext.innerHTML = `${drink.size.small.name}<br><b>${drink.size.small.price},-</b>`;
+  mediumtext.innerHTML = `${drink.size.medium.name}<br><b>${drink.size.medium.price},-</b>`;
+  largetext.innerHTML = `${drink.size.large.name}<br><b>${drink.size.large.price},-</b>`;
+
+  displayMenuCard.style.display = "block";
+
+  // Select size of drink when click on image
+  let selectedSize;
+  document.querySelectorAll(".size-btn").forEach(
+    (element) =>
+      (element.onclick = (event) => {
+        selectedSize = element.name;
+        setActive(element, "size-btn");
+      })
+  );
+
+  // onclick "add to order"
+  document.querySelector("#menuItemFooter").onclick = (event) => {
+    if (selectedSize) {
+      addDrinkToCart(name, selectedSize, orderQuantity);
+      refreshShoppingCart();
+      closeModal();
+    } else {
+      error.innerHTML = "Du må velge en størrelse!";
+    }
+  };
+}
+
+/**
+ * Opens the desserts modal.
+ * @param {string} name name of the dessert as it is in the desserts.js
+ */
+function openDessertModal(name) {
+  const dessert = getDessert(name);
+
+  orderQuantity = 1;
+  displayQuantity.innerHTML = `${orderQuantity}`;
+  itemName.innerHTML = dessert.name;
+  itemPhoto.innerHTML = `<img src="Images/${dessert.image.src}" alt="${dessert.image.src}" id = "menuPhoto" />`;
+  drinkSize.style.display = "none";
+  foodPriceDiv.style.display = "block";
+  foodPriceTxt.innerHTML = `<b>${dessert.price},-</b>`;
+
+  //after the user has clicked the button and the info has been generated, the cardview becomes visible
+  displayMenuCard.style.display = "block";
+
+  // onclick "add to order"
+  document.querySelector("#menuItemFooter").onclick = (event) => {
+    addDessertToCart(name, orderQuantity);
+    refreshShoppingCart()
+    closeModal();
+  };
+}
+
+/**
+ * Set active class to element and remove to all other classes specified
+ * @param {HTMLElement} selectedItem element to add active class to
+ * @param {string} clearActiveClass class of elements to remove active class element to
+ */
+function setActive(selectedItem, clearActiveClass) {
+  document
+    .querySelectorAll(`.${clearActiveClass}`)
+    .forEach((element) => element.classList.remove("active"));
+  selectedItem.classList.add("active");
+}
+
+/**
+ * Close the modal. Clears errors and selected drink sizes.
+ */
+function closeModal() {
+  displayMenuCard.style.display = "none";
+  error.innerHTML = "";
+  document
+    .querySelectorAll(`.size-btn`)
+    .forEach((element) => element.classList.remove("active"));
+}
+
+//Close card view when exit button is clicked
+closeButton.onclick = function () {
+  closeModal();
+};
+
+//Close card view when you click outside of the card
+window.onclick = function (event) {
+  if (event.target == displayMenuCard) {
+    closeModal();
+  }
+};
+
+/**
+ * subtract the amount of items you want to order
+ * */
+function subtractQuantity() {
+  if (orderQuantity === 1) {
+    error.innerHTML = "Antall kan ikke være lavere enn 1!";
+  } else {
+    orderQuantity--;
+    displayQuantity.innerHTML = `${orderQuantity}`;
+    error.innerHTML = "";
+  }
+}
+
+/**
+ * add the amount of items you want to order
+ * */
+function addQuantity() {
+  orderQuantity++;
+  displayQuantity.innerHTML = `${orderQuantity}`;
+  error.innerHTML = "";
+}
